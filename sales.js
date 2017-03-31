2e8cdf1b56df1492172a4e82f1364762429ba128
Daniel Johnson <djohnson93@mail.csuchico.edu>
2017-03-30 22:07:52 -0700

    fixed NaN

--- sales.js
+++ sales.js
@@ -14,6 +14,14 @@ function addItem()
   // update the innerHTML of the span with the id "subtotal" to be dollars
   // update the value of the input with the id "price" to be an empty string
   // update a cookie called "preTax" with the value of runningTotal
+  if (newItem= NaN)
+  {
+    document.getElementById("price").innerHTML = "Enter price as number";
+    document.getElementById("subtotal").classList.add("has-error");
+
+  }else {
+    document.getElementById("subtotal").classList.add("has-success");
+  }
 }
 
 //takes a number and gives a string with the number displayed as USD currency
@@ -23,22 +31,22 @@ function asCurrency(val)
 }
 
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
-function setCookie(cname, cvalue, exdays) {
+function setCookie(pretax, subtotal, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = pretax + "=" + subtotal + ";" + expires + ";path=/";
 }
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
-function getCookie(cname) {
-    var name = cname + "=";
+function getCookie(pretax) {
+    var subtotal = pretax + "price";
     var ca = document.cookie.split(';');
     for(var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
+        if (c.indexOf(subtotal) == 0) {
             return c.substring(name.length, c.length);
         }
     }
